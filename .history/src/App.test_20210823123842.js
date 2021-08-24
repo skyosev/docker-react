a8a6bd04902db879c7341f3cfea2bf009df1dc22
Stoyan Kyosev <stoyankyosev@gmail.com>
2021-08-24 23:48:07 +0300

    initial commit

--- .history/src/App.test_20210823123842.js
+++ .history/src/App.test_20210823123842.js
@@ -7,6 +7,7 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
